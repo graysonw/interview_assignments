35db2d0d2b85dffc6033130516f10e62c7093def
Grayson Williams <graysonw@gmail.com>
2019-11-19 17:47:14 -0800

    Update tides.py

--- tides.py
+++ tides.py
@@ -30,13 +30,24 @@ for arg in args:
             print(sunrise, sunset)
     future_tides = this_soup.findAll('div', {"class": "tide-table"})
     for tide in future_tides:
-        print(tide.findAll("h4"))
-        #print(str(tide.td))
-        if "Sunrise" in str(tide.td):
-            print("foo")
-        sun_search = re.search('Sunrise:<span> (.*)</span>', str(tide), re.IGNORECASE)
-        #if sun_search:
-        #    print(sun_search.group(1).strip())
+        tide_date = tide.findAll("h4")
+        tide_date_search = re.search('Tide Times for .*\: (.*)</h4>', str(tide_date), re.IGNORECASE)
+
+        if tide_date_search:
+            print(tide_date_search.group(1).strip())
+
+        sun_info = tide.findAll("table", {"class":"tide-table__tide-data--sun-moon not_in_print"})[0].findAll("td")
+        #print(sun_info[0].findAll("td"))
+        #if "Sunrise" in str(tide.td):
+        #    print("foo")
+        sunrise = sun_info[0].findAll("span")
+        sunset = sun_info[1].findAll("span")
+        sunrise_search = re.search('<span>(.*)<.*', str(sunrise[0]), re.IGNORECASE)
+        if sunrise_search:
+            print(sunrise_search.group(1).strip())
+        sunset_search = re.search('<span>(.*)<.*', str(sunset[0]), re.IGNORECASE)
+        if sunset_search:
+            print(sunset_search.group(1).strip())
     #print(tides[0])
     #print(tides[1])
     #for tide in tides:
